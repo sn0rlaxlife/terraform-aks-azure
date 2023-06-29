cbbdcc6a4a4f80933afb9c83dc499ef0cfd48472
sn0rlax <79283899+sn0rlaxlife@users.noreply.github.com>
2023-06-29 08:54:02 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,14 +47,17 @@ module "network" {
   source              = "Azure/network/azurerm"
   resource_group_name = azurerm_resource_group.aks.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+  vnet_name           = "aks-west"
+  address_space       = var.aks_vnet_address_space
 
-  subnet_service_endpoints = {
-    "subnet1" : ["Microsoft.Sql"],
-    "subnet2" : ["Microsoft.Sql"],
-    "subnet3" : ["Microsoft.Sql"]
-  }
+ subnets = [
+    {
+      name : var.default_node_pool_subnet_name
+      address_prefixes : var.default_node_pool_subnet_address_prefix
+      enforce_private_link_endpoint_network_policies : true
+      enforce_private_link_service_network_policies : false
+    }
+   ]
   use_for_each = true
   tags = {
     environment = "dev"
